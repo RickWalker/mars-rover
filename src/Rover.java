e11683988bba32085c642a8af58b9ee42f1a4574
Rick Walker <rick.t.walker@gmail.com>
2014-04-28 13:36:27 +0100

    First two requirements - moving and turning.
    No grid size yet, because that’s not needed until we start wrapping.

---
+++ src/Rover.java
@@ -0,0 +1,108 @@
+import java.awt.Point;
+
+public class Rover {
+	Point position;
+	char direction;
+
+	Rover(int x, int y, char d) {
+		this.position = new Point(x, y);
+		this.direction = d;
+	}
+
+	public char getDirection() {
+		return direction;
+	}
+
+	public void executeCommands(String commandString) {
+		// execute each of the commands in turn
+		for (char c : commandString.toCharArray()) {
+			// process command
+			switch (c) {
+			case 'F':
+				moveForward();
+				break;
+			case 'B':
+				moveBackward();
+				break;
+			case 'L':
+				turnLeft();
+				break;
+			case 'R':
+				turnRight();
+				break;
+			}
+		}
+	}
+
+	void moveForward() {
+		switch (this.direction) {
+		case 'N':
+			position.translate(0, 1);
+			break;
+		case 'S':
+			position.translate(0, -1);
+			break;
+		case 'E':
+			position.translate(1, 0);
+			break;
+		case 'W':
+			position.translate(-1, 0);
+			break;
+		}
+	}
+
+	void moveBackward() {
+		switch (this.direction) {
+		case 'N':
+			position.translate(0, -1);
+			break;
+		case 'S':
+			position.translate(0, 1);
+			break;
+		case 'E':
+			position.translate(-1, 0);
+			break;
+		case 'W':
+			position.translate(1, 0);
+			break;
+		}
+	}
+
+	void turnLeft() {
+		switch (this.direction) {
+		case 'N':
+			this.direction = 'W';
+			break;
+		case 'W':
+			this.direction = 'S';
+			break;
+		case 'S':
+			this.direction = 'E';
+			break;
+		case 'E':
+			this.direction = 'N';
+			break;
+		}
+	}
+
+	void turnRight() {
+		switch (this.direction) {
+		case 'N':
+			this.direction = 'E';
+			break;
+		case 'E':
+			this.direction = 'S';
+			break;
+		case 'S':
+			this.direction = 'W';
+			break;
+		case 'W':
+			this.direction = 'N';
+			break;
+		}
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+}
--- tests/TestRover.java
+++ tests/TestRover.java
@@ -1,13 +1,172 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import java.awt.Point;
 
 public class TestRover {
 
+	// simple creation tests first
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void WhenCreateRoverAt00North_ThenPositionDirectionAreCorrectAt00() {
+		Rover testRover = new Rover(0, 0, 'N');
+		assertEquals("Position of new rover should be 0, 0", new Point(0, 0),
+				testRover.getPosition());
+		assertEquals("Direction of new rover should be N", 'N',
+				testRover.getDirection());
 	}
+
+	@Test
+	public void WhenCreateRoverAt15North_ThenPositionDirectionAreCorrectAt15() {
+		Rover testRover = new Rover(1, 5, 'N');
+		assertEquals("Position of new rover should be 1, 5", new Point(1, 5),
+				testRover.getPosition());
+		assertEquals("Direction of new rover should be N", 'N',
+				testRover.getDirection());
+	}
+
+	// then some movement ones
+
+	@Test
+	public void WhenMoveForwardFrom00N_ThenPositionDirectionAreCorrectAt01N() {
+		Rover testRover = new Rover(0, 0, 'N');
+		testRover.executeCommands("F");
+		assertEquals(
+				"Position of rover after moving forwards from 0, 0 N should be 0, 1",
+				new Point(0, 1), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after moving forwards from 0, 0 N should be N",
+				'N', testRover.getDirection());
+	}
+
+	@Test
+	public void WhenMoveBackwardFrom01N_ThenPositionDirectionAreCorrectAt00N() {
+		Rover testRover = new Rover(0, 1, 'N');
+		testRover.executeCommands("B");
+		assertEquals(
+				"Position of rover after moving backwards from 0, 1 N should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after moving backwards from 0, 1 N should be N",
+				'N', testRover.getDirection());
+	}
+
+	// then some turning ones
+	@Test
+	public void WhenTurnLeftFrom00N_ThenNewPositionDirectionIs00W() {
+		Rover testRover = new Rover(0, 0, 'N');
+		testRover.executeCommands("L");
+
+		assertEquals(
+				"Position of rover after turning left from 0, 0 N should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning left from 0, 0 N should be W",
+				'W', testRover.getDirection());
+
+	}
+	
+	@Test
+	public void WhenTurnLeftFrom00W_ThenNewPositionDirectionIs00S() {
+		Rover testRover = new Rover(0, 0, 'W');
+		testRover.executeCommands("L");
+
+		assertEquals(
+				"Position of rover after turning left from 0, 0 W should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning left from 0, 0 W should be S",
+				'S', testRover.getDirection());
+	}
+	
+	@Test
+	public void WhenTurnLeftFrom00S_ThenNewPositionDirectionIs00E() {
+		Rover testRover = new Rover(0, 0, 'S');
+		testRover.executeCommands("L");
+
+		assertEquals(
+				"Position of rover after turning left from 0, 0 S should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning left from 0, 0 S should be E",
+				'E', testRover.getDirection());
+	}
+	
+	@Test
+	public void WhenTurnLeftFrom00E_ThenNewPositionDirectionIs00N() {
+		Rover testRover = new Rover(0, 0, 'E');
+		testRover.executeCommands("L");
+
+		assertEquals(
+				"Position of rover after turning left from 0, 0 E should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning left from 0, 0 E should be N",
+				'N', testRover.getDirection());
+	}
+	
+	@Test
+	public void WhenTurnRightFrom00N_ThenNewPositionDirectionIs00E() {
+		Rover testRover = new Rover(0, 0, 'N');
+		testRover.executeCommands("R");
+
+		assertEquals(
+				"Position of rover after turning right from 0, 0 N should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning right from 0, 0 N should be E",
+				'E', testRover.getDirection());
+
+	}
+	
+	@Test
+	public void WhenTurnRightFrom00E_ThenNewPositionDirectionIs00S(){
+		Rover testRover = new Rover(0, 0, 'E');
+		testRover.executeCommands("R");
+
+		assertEquals(
+				"Position of rover after turning right from 0, 0 E should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning right from 0, 0 E should be S",
+				'S', testRover.getDirection());
+	}
+	
+	@Test
+	public void WhenTurnRightFrom00S_ThenNewPositionDirectionIs00W(){
+		Rover testRover = new Rover(0, 0, 'S');
+		testRover.executeCommands("R");
+
+		assertEquals(
+				"Position of rover after turning right from 0, 0 S should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning right from 0, 0 S should be W",
+				'W', testRover.getDirection());
+	}
+	
+	@Test
+	public void WhenTurnRightFrom00W_ThenNewPositionDirectionIs00N(){
+		Rover testRover = new Rover(0, 0, 'W');
+		testRover.executeCommands("R");
+
+		assertEquals(
+				"Position of rover after turning right from 0, 0 W should be 0, 0",
+				new Point(0, 0), testRover.getPosition());
+		assertEquals(
+				"Direction of rover after turning right from 0, 0 W should be N",
+				'N', testRover.getDirection());
+	}	
+	
+	//finally, some combo tests
+	@Test
+	public void WhenFFRFF_From_00N_ThenPositionDirectionShouldBe22E(){
+		Rover testRover = new Rover(0,0, 'N');
+		testRover.executeCommands("FFRFF");
+		
+		assertEquals("Rover from 00N after FFRFF should be at position 2,2", new Point(2,2), testRover.getPosition());
+		assertEquals("Rover from 00N after FFRFF should be pointing E", 'E', testRover.getDirection());
+	}
+	
 
 }
